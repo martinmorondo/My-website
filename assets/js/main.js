0cb75ccc1548172e7dfd4577f4dafef17b7af084
Martín Morondo <martinmorondo@gmail.com>
2023-01-04 20:11:34 -0300

    I added a new section of projects, with their respective styles and functionalities

--- assets/js/main.js
+++ assets/js/main.js
@@ -252,7 +252,86 @@
 
 })()
 
+new Swiper('.clients-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 40
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 60
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 80
+    },
+    992: {
+      slidesPerView: 6,
+      spaceBetween: 120
+    }
+  }
+});
 
+new Swiper('.slides-1', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+new Swiper('.slides-3', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 40
+    },
+
+    1200: {
+      slidesPerView: 3,
+    }
+  }
+});
 
 
 
